d715c1cc226d68909472315c911ebb91bbbe676c
Naroz Ezzat <narozezzat10@gmail.com>
2023-07-23 17:30:41 +0300

    add icons in admin

--- src/Componant/Admin/AdminSideBar.js
+++ src/Componant/Admin/AdminSideBar.js
@@ -1,10 +1,24 @@
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function AdminSideBar() {
+
+    const SignOut = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("userId")
+        localStorage.removeItem("Admin")
+    }
+
     return (
         <>
             <div className='d-flex flex-column'>
+
+                <div className='seller-text mb-3 mt-5 border-bottom p-2 mx-auto text-center'>
+                    <FontAwesomeIcon icon={faHouse}  className='text-warning'/>
+                    <span className='mx-1'>Admin DashBoard</span>
+                </div>
 
                 <Link to="/Admin/Orders" style={{textDecoration: 'none'}}>
                     <div className='admin-side-text mt-3 border-bottom p-2 mx-auto text-center'>
@@ -33,6 +47,12 @@ function AdminSideBar() {
                 <Link to="/Admin/AddProducts" style={{textDecoration: 'none'}}>
                     <div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
                         Show Users
+                    </div>
+                </Link>
+
+                <Link to="/MainPage" style={{textDecoration: 'none'}} onClick={SignOut}>
+                    <div className='seller-text mb-3 mt-4 border-bottom p-2 mx-auto text-center'>
+                        SignOut
                     </div>
                 </Link>
                 
